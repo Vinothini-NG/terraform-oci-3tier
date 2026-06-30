eca248207802d4e5dd7f408163c60e23591349cb
Vino <nvinothin21@gmail.com>
2026-06-30 13:17:50 +0530

    Configure remote backend

---
+++ terraform/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "oci-3tier/terraform.tfstate"
+    region = "ap-sydney-1"
+
+    endpoints = {
+      s3 = "https://sdlvq8nmuuve.compat.objectstorage.ap-sydney-1.oraclecloud.com"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    use_path_style              = true
+  }
+}
