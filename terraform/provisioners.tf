addf16b8e2810d8a8fd9ab74dce3a33453d1e9ed
Vino <nvinothin21@gmail.com>
2026-07-01 07:06:46 +0530

    connection test

--- terraform/provisioners.tf
+++ terraform/provisioners.tf
@@ -2,6 +2,7 @@ resource "null_resource" "bastion_to_private_test" {
 
   depends_on = [
     oci_core_instance.bastion_host
+    oci_core_instance.application_node1
   ]
 
   connection {
@@ -17,6 +18,9 @@ resource "null_resource" "bastion_to_private_test" {
       "whoami",
       "ping -c 1 google.com",
       "mkdir -p ~/.ssh"
+      "cat > ~/.ssh/private_key <<'EOF'\n${var.ssh_private_key}\nEOF",
+      "chmod 600 ~/.ssh/private_key",
+      "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 -i ~/.ssh/private_key opc@${oci_core_instance.application_node1.private_ip} 'echo CONNECTED && hostname && date' > /tmp/connectivity_result.txt 2>&1; echo $? > /tmp/connectivity_exit_code.txt",
     ]
   }
 }
