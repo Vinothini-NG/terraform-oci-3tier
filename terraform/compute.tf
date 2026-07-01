12512146b63298e72f690671314a9ce35bcbc3b2
Vino <nvinothin21@gmail.com>
2026-07-01 07:03:34 +0530

    added application node1

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -40,3 +40,31 @@ resource "oci_core_instance" "bastion_host" {
     ssh_authorized_keys = var.ssh_public_key
     }
 }
+
+#CREATING APPLICATION NODE1
+resource "oci_core_instance" "application_node1" {
+  availability_domain = "eaWm:AP-SYDNEY-1-AD-1"
+  compartment_id      = var.compartment_ocid
+  display_name        = "application-node1-tf-github"
+  shape               = "VM.Standard.E5.Flex"
+
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 12
+  }
+
+  create_vnic_details {
+    subnet_id              = oci_core_subnet.private_subnet.id
+    assign_public_ip       = false
+    display_name           = "application-vnic"
+    hostname_label         = "appnode1"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = data.oci_core_images.oracle_linux.images[0].id
+  }
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+    }
+}
