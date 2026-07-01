cb237355de61f851aa3e11dce6740298d22c5d73
Vino <nvinothin21@gmail.com>
2026-07-01 07:21:56 +0530

    connection test3

--- terraform/provisioners.tf
+++ terraform/provisioners.tf
@@ -20,7 +20,7 @@ resource "null_resource" "bastion_to_private_test" {
       "mkdir -p ~/.ssh",
       "cat > ~/.ssh/private_key <<'EOF'\n${var.ssh_private_key}\nEOF",
       "chmod 600 ~/.ssh/private_key",
-      "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 -i ~/.ssh/private_key opc@${oci_core_instance.application_node1.private_ip} 'echo CONNECTED && hostname && date && ping -c 3 google.com' > /tmp/connectivity_result.txt 2>&1; echo $? > /tmp/connectivity_exit_code.txt",
+      "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 -i ~/.ssh/private_key opc@${oci_core_instance.application_node1.private_ip} 'echo CONNECTED && hostname && date && ping -c 3 google.com'",
     ]
   }
 }
