ccba01db718276b42441d475256112ca51d58aed
Vino <nvinothin21@gmail.com>
2026-07-01 12:21:41 +0530

    add output1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "load_balancer_public_ip" {
   value = oci_load_balancer_load_balancer.load_balancer_tf.ip_address_details[0].ip_address
 }
-
-output "wallet_par_url" {
-  value = "https://objectstorage.ap-sydney-1.oraclecloud.com${oci_objectstorage_preauthrequest.wallet_par.access_uri}"
-}
